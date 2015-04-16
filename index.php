a03a3b2d0bb2586d2475ca8f81afe1a9bf9078cb
Edwin Corzo <edwin11ramirez@gmail.com>
2015-04-16 12:02:19 -0700

    milestone 6

--- index.php
+++ index.php
@@ -2,11 +2,18 @@
 <html>
 <head>
 	<title>Edwin's To-Do List</title>
-	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">  
 </head>
 <body>
 	<div class="wrap">
-		
+	  <div class="task-list">
+	    <ul>
+	    	<?php require("includes/connect.php"); ?>
+	    </ul>	
+	</div>
+	<form class="add-new-task" autocomplete="off">
+		<input type="text" name="new-task" placeholder="Add new item..."/>
+	</form>
 	</div>
 </body>
 </html>
